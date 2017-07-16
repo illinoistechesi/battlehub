f385a86ea0c00d65b278abf5105f2c14fbc73375
Vinesh Kannan <vingkan@gmail.com>
2017-07-16 09:36:26 +0000

    Add MissionRunner

---
+++ MissionRunner.java
@@ -0,0 +1,50 @@
+package battlehub;
+import battleship.core.*;
+import battleship.ships.*;
+import battleship.games.*;
+
+public class MissionRunner {
+    
+    public static Class<? extends Ship> PLAYER_CLASS = esi17.vkannan3.KannanShip.class;
+    
+    public static void main(String[] args) {
+        
+        try {
+            
+            int n = Integer.parseInt(args[0]);
+            Game mission = null;
+            if (args.length >= 2) {
+                String className = args[1];
+                Class<?> c = Class.forName(className);
+                @SuppressWarnings("unchecked")
+                Class<? extends Ship> playerClass = (Class<? extends Ship>) c;
+                mission = getMission(n, playerClass);
+            } else {
+                mission = getMission(n, PLAYER_CLASS);
+            }
+            if (mission != null) {
+                mission.setSeed(42);
+                mission.run();
+                
+            } else {
+                System.out.println("Could not find Mission #" + n);
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public static Game getMission(int n, Class<? extends Ship> pc) {
+        switch (n) {
+            case 1:
+                return new Mission01(pc);
+            case 2:
+                return new Mission02(pc);
+            default:
+                return null;
+        }
+    }
+    
+}
